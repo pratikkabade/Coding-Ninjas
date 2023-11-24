a64dba0c277a0d91c5936aefa4488aab8c14c057
Pratik <thisispratikkabade@gmail.com>
2023-11-24 14:00:00 +0530

    Backup #2

---
+++ Fundamentals of Web Development/02-Final-Project-Musify/src/assets/songs.js
@@ -0,0 +1,126 @@
+let songs_col_song1 = document.querySelectorAll('.songs-col-song1')
+
+songs_col_song1.forEach((element) => {
+    element.innerHTML = `
+            <a href="#" id="songs-col-song">
+                <img src="./assets/1989.jpg" alt="Songs" />
+                <div id="songs-col-song-details">
+                    <span id="song-name">Song Name</span>
+                    <span id="artist-name">Artist Name</span>
+                </div>
+            </a>
+            `
+})
+
+let songs_col_song2 = document.querySelectorAll('.songs-col-song2')
+
+songs_col_song2.forEach((element) => {
+    element.innerHTML = `
+            <a href="#" id="songs-col-song">
+                <img src="./assets/Fearless TV.jpg" alt="Songs" />
+                <div id="songs-col-song-details">
+                    <span id="song-name">Song Name</span>
+                    <span id="artist-name">Artist Name</span>
+                </div>
+            </a>
+            `
+})
+
+let songs_col_song3 = document.querySelectorAll('.songs-col-song3')
+
+songs_col_song3.forEach((element) => {
+    element.innerHTML = `
+            <a href="#" id="songs-col-song">
+                <img src="./assets/Lover.jpg" alt="Songs" />
+                <div id="songs-col-song-details">
+                    <span id="song-name">Song Name</span>
+                    <span id="artist-name">Artist Name</span>
+                </div>
+            </a>
+            `
+})
+
+let songs_col_song4 = document.querySelectorAll('.songs-col-song4')
+
+songs_col_song4.forEach((element) => {
+    element.innerHTML = `
+            <a href="#" id="songs-col-song">
+                <img src="./assets/Midnights.jpg" alt="Songs" />
+                <div id="songs-col-song-details">
+                    <span id="song-name">Song Name</span>
+                    <span id="artist-name">Artist Name</span>
+                </div>
+            </a>
+            `
+})
+
+let songs_col_song5 = document.querySelectorAll('.songs-col-song5')
+
+songs_col_song5.forEach((element) => {
+    element.innerHTML = `
+            <a href="#" id="songs-col-song">
+                <img src="./assets/Red TV.jpg" alt="Songs" />
+                <div id="songs-col-song-details">
+                    <span id="song-name">Song Name</span>
+                    <span id="artist-name">Artist Name</span>
+                </div>
+            </a>
+            `
+})
+
+let songs_col_song6 = document.querySelectorAll('.songs-col-song6')
+
+songs_col_song6.forEach((element) => {
+    element.innerHTML = `
+            <a href="#" id="songs-col-song">
+                <img src="./assets/Reputation.jpg" alt="Songs" />
+                <div id="songs-col-song-details">
+                    <span id="song-name">Song Name</span>
+                    <span id="artist-name">Artist Name</span>
+                </div>
+            </a>
+            `
+})
+
+let songs_col_song7 = document.querySelectorAll('.songs-col-song7')
+
+songs_col_song7.forEach((element) => {
+    element.innerHTML = `
+            <a href="#" id="songs-col-song">
+                <img src="./assets/Speak Now TV.jpg" alt="Songs" />
+                <div id="songs-col-song-details">
+                    <span id="song-name">Song Name</span>
+                    <span id="artist-name">Artist Name</span>
+                </div>
+            </a>
+            `
+})
+
+
+let queue_song = document.querySelectorAll('.queue-song');
+
+queue_song.forEach((element) => {
+    element.innerHTML = `
+          <a href="#" id="songs-col-song">
+            <img src="./assets/1989.jpg" alt="Songs" />
+            <div id="songs-col-song-details">
+              <span id="song-name">Song Name</span>
+              <span id="artist-name">Artist Name</span>
+            </div>
+          </a>`
+});
+
+
+let album_songs = document.querySelectorAll('.page-album-song');
+
+album_songs.forEach((element) => {
+    element.innerHTML = `
+          <a href="#" id="songs-col-song">
+            <img src="./assets/Fearless TV.jpg" alt="Songs" />
+            <div id="songs-col-song-details">
+              <span id="song-name">Song Name</span>
+              <span id="artist-name">Artist Name</span>
+            </div>
+          </a>`
+});
+
---
+++ Fundamentals of Web Development/02-Final-Project-Musify/src/script.js
@@ -0,0 +1,14 @@
+
+
+// change songs of id "songs" 
+document.getElementById("as").innerHTML = `
+    <div class="song">
+        <img src="./assets/1989.jpg" alt="1989" />
+        <h2>Love Story</h2>
+        <h3>Taylor Swift</h3>
+        <audio src="./assets/love-story.mp3" controls></audio>
+    </div>
+    `
+
+
+
